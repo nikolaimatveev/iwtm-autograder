a260c9b47f5b15b196190b6deaf0a26dba5e1859
Konstantin Stepanov <k.stepanov98@yandex.ru>
2020-05-05 18:17:16 +0300

    new event service

--- app/services.py
+++ app/services.py
@@ -16,28 +16,61 @@ class EventService:
         with open(filename, encoding='utf-8') as file:
             reader = csv.DictReader(file, delimiter=';')
             for row in reader:
-                events.append(row)
+                event = {}
+                event['task_number'] = row['task_number']
+                event['filename'] = row['filename']
+                event['sender'] = row['sender']
+                event['recipient'] = row['recipient']
+                event['policies'] = row['policies'].split(',')
+                event['verdict'] = row['verdict']
+                event['violation_level'] = row['violation_level']
+                event['tags'] = row['tags'].split(',')
+                events.append(event)
         return events
     
-    def find_by_complex_id(events, filename, sender, recipient):
+    def find_all_by_id(self, events, filename, sender, recipient):
         found_events = []
         for event in events:
-            if event.filename == filename and event.sender == sender and event.recipient == recipient:
+            subject = json.loads(event['PREVIEW_DATA'])['subject'].split(', ')
+            event_sender = subject[0].split(':')[1].strip()
+            event_recipient = subject[1].split(':')[1].strip()
+            event_filename = subject[2].split(':')[1].strip()
+            event['sender'] = event_sender
+            event['recipient'] = event_recipient
+            event['filename'] = event_filename
+            if event_filename == filename and event_sender == sender and event_recipient == recipient:
                 found_events.append(event)
         return found_events
+
+    def align_real(self, real_events, template_events):
+        result = []
+        for event in template_events:
+            found_events = self.find_all_by_id(real_events, event['filename'], event['sender'], event['recipient'])
+            for e in found_events:
+                result.append(e)
+        return result
+    
+    def align_template(template_events, real_events):
+        result = []
+        for event in template_events:
+            found_events = self.find_all_by_id(real_events, event.filename, event.sender, event.recipient)
+            result.append(event)
+            empty_event = {}
+            empty_event['filename'] = 'null'
+            for _ in found_events:
+                result.append(empty_event)
+        return result
 
     def get_real(self, filename):
         data = []
         with open(filename, encoding='utf-8') as json_file:
             data = json.load(json_file)
         return data['data']
-    
-    # Входные данные - лист заданий для реальных событий, лист заданий для шаблонных событий
-    # Порядок политик, тэгов в шаблоне и факте может отличаться, как сравнивать?
+
     def compare(self, template_events, real_events):
         deltas = []
-        template_real_events_zipped = zip(template_events, real_events)
-        for template_event, real_event in template_real_events_zipped:
+        delta_id = 1
+        for template_event, real_event in zip(template_events, real_events):
             event_delta = {}
             diff_policies = []
             for policy in real_event['policies']:
@@ -51,16 +84,23 @@ class EventService:
                     diff_tags.append(tag['DISPLAY_NAME'])
             event_delta['tags'] = diff_tags
 
-            event_delta['verdict'] = (
+            tmp_diff = {}
+            tmp_diff['id'] = delta_id
+            delta_id += 1
+            tmp_diff['name'] = (
                 real_event['VERDICT']
                 if template_event['verdict'] != real_event['VERDICT']
                 else 'ok'
             )
+
+            event_delta['verdict'] = tmp_diff
+
 
             event_delta['violation_level'] = (
                 real_event['VIOLATION_LEVEL']
                 if template_event['violation_level'] != real_event['VIOLATION_LEVEL']
                 else 'ok'
             )
+            print(event_delta)
             deltas.append(event_delta)
         return deltas
--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 import datetime
+import os.path
 
 from .models import Event
 from .services import EventService
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Шаблонные события - просто выводим в таблицу
 # Реальные события - преобразовать к упрощенному виду
@@ -11,33 +13,49 @@ from django.http import HttpResponse
 # Порядок событий в двух таблицах должен совпадать
 # Проблема: одному шаблонному событию может соответствовать несколько реальных
 # В этом случае политика неправильная в том случае, если все события неверные
-
-files = ['report.jpg', 'report.jpg', 'nepravilno.rtf', 
-         'Wordlskilz.rtf', 'dogovor.doc', 'dogovor_direktor.doc',
-         'dogovor_pechat.pdf', 'anketa.docx', 'anketa_pechat.pdf',
-         'anketa.docx', 'anketa_pechat.pdf', 'promoall',
-         'promotwo', 'promoall', 'promotwo',
-         'catoo', 'catoo_reduced', 'catoo',
-         'catoo_reduced', 'evil_routes', 'routes',
-         'evil_routes', 'routes', 'candy',
-         'candy', 'taxi', 'dbfull',
-         'dbsmall', 'dbfull', 'dbsmall',
-         'email', 'ne-email', 'strogo_konfidencialno',
-         'konfidencialno', 'strogo_konfidencialno_m', 'Win1.zip']
  
 def index(request):
+    if request.method=='POST' and 'template' in request.POST:
+        print('saving template')
+        save_template_file(request.FILES['template_file'])
+
+    if request.method=='POST' and 'iw_ip' in request.POST:
+        print('loading events from iw')
+    
     event_service = EventService()
-    template_events = event_service.get_template('app/static/template.csv')
+    template_events = []
+    template_path = 'app/static/upload/template.csv'
     real_events = event_service.get_real('app/static/sample-events.json')
-    real_events = sorted(real_events, key=sent_date_sorting)
-    deltas = event_service.compare(template_events, real_events)
+    
+    #real_events = sorted(real_events, key=sent_date_sorting)
+    deltas = []
+    if os.path.isfile(template_path):
+        template_events = event_service.get_template(template_path)
+        real_events = event_service.align_real(real_events, template_events)
+        deltas = event_service.compare(template_events, real_events)
+    
     context = {'template_events': template_events,
-                'real_events_with_deltas': zip(real_events, deltas, files)}
-    return render(request, 'app/index.html', context)
+                'real_events_with_deltas': zip(real_events, deltas)}
+    return render(request, 'app/test.html', context)
+
+def add_delta(request):
+    print(request.GET['id'])
+    return HttpResponseRedirect('/')
+
+def remove_delta(request):
+    return HttpResponseRedirect('/')
+
+def save_template_file(file):
+    with open('app/static/upload/template.csv', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
 
 def test(request):
     event = Event('cat.jpg', 'Sales', 'HR', ['Policy 3, Policy 5'], 'Allowed', 'Low', 'Tag 1')
     context = {'event': event}
+    event_service = EventService()
+    real_events = event_service.get_real('app/static/sample-events.json')
+    event_service.find_all_by_id(real_events, 'catoo.jpg', 'IT', 'External')
     return render(request, 'app/test.html', context)
 
 def sent_date_sorting(event):
